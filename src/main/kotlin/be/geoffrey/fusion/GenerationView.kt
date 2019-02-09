a20310713e187ca875cac52ab9b6df11056a85fb
Geoffrey De Vylder <geoffreydv@gmail.com>
2019-02-09 11:22:54 +0100

    First attempt, writing a string

--- src/main/kotlin/be/geoffrey/fusion/GenerationView.kt
+++ src/main/kotlin/be/geoffrey/fusion/GenerationView.kt
@@ -12,12 +12,13 @@ import com.vaadin.flow.router.Route
 // Manage both Java example and XML example at the same time
 // Just to try to improve the overall design
 
-interface Structure
+interface Structure {
+    val name: String
+}
 
-data class ComplexStructure(val name: String,
-                            val fields: List<Field>) : Structure
+data class ComplexStructure(override val name: String, val fields: List<Field>) : Structure
 
-data class SimpleStructure(val name: String) : Structure
+data class SimpleStructure(override val name: String) : Structure
 
 data class Field(val name: String, val type: Structure)
 
@@ -25,14 +26,15 @@ data class Field(val name: String, val type: Structure)
 @StyleSheet("/style.css")
 class GenerationView : VerticalLayout() {
 
-    init {
-        val knownStructures = HashMap<String, Structure>()
+    private val knownStructures = HashMap<String, Structure>()
 
+    init {
         knownStructures["String"] = SimpleStructure("String")
-        knownStructures["Person"] = ComplexStructure("Person", listOf(
-                Field("firstName", knownStructures["String"]!!),
-                Field("lastName", knownStructures["String"]!!)
-        ))
+        knownStructures["Person"] = ComplexStructure("Person",
+                listOf(
+                        Field("firstName", knownStructures["String"]!!),
+                        Field("lastName", knownStructures["String"]!!)
+                ))
 
         val outputBox = TextArea()
         outputBox.isReadOnly = true
@@ -40,6 +42,9 @@ class GenerationView : VerticalLayout() {
         val rootCombo = ComboBox<String>()
         rootCombo.setItems(knownStructures.keys)
         rootCombo.addValueChangeListener { outputBox.value = generateOutput(it.value) }
+
+        outputBox.width = "800px"
+        outputBox.height = "400px"
 
         add(
                 H1("Codename: Fusion"),
@@ -50,5 +55,23 @@ class GenerationView : VerticalLayout() {
         )
     }
 
-    private fun generateOutput(rootType: String) = "My Output..."
+    private fun generateOutput(rootType: String): String {
+        val type = knownStructures[rootType]
+
+        // First write a variable assignment, this is specific to the language
+        // If Java
+
+        var output = ""
+        output += "${type!!.name} tmp = "
+
+        if (type is SimpleStructure) {
+            if (type.name == "String") {
+                output += "\"StringValue\";"
+            }
+        } else if (type is ComplexStructure) {
+            output += "new ${type.name}();"
+        }
+
+        return output;
+    }
 }
