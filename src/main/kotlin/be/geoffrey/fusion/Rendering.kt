4da3d4b34e887afa2a42c61102a84e41b02cfabd
Geoffrey De Vylder <geoffreydv@gmail.com>
2019-05-18 15:51:36 +0200

    Removed some junk

--- src/main/kotlin/be/geoffrey/fusion/Rendering.kt
+++ src/main/kotlin/be/geoffrey/fusion/Rendering.kt
@@ -10,6 +10,14 @@ import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
 
+data class RegexValueForType(val type: QName, val valueToRender: String)
+
+data class RenderingConfig(private val regexValueForTypes: List<RegexValueForType> = listOf()) {
+    fun getRegexValueForType(type: QName): String? {
+        return regexValueForTypes.filter { it.type == type }.map { it.valueToRender }.firstOrNull()
+    }
+}
+
 interface Renderer {
 
     fun render(element: TopLevelElement,
--- src/main/kotlin/be/geoffrey/fusion/RenderingConfig.kt
+++
@@ -1,9 +0,0 @@
-package be.geoffrey.fusion
-
-data class RegexValueForType(val type: QName, val valueToRender: String)
-
-data class RenderingConfig(private val regexValueForTypes: List<RegexValueForType> = listOf()) {
-    fun getRegexValueForType(type: QName): String? {
-        return regexValueForTypes.filter { it.type == type }.map { it.valueToRender }.firstOrNull()
-    }
-}
